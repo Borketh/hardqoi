b287b8c0481514735506c1b9f39b4272a123c70e
floof <does.not.kompute@gmail.com>
2022-07-18 14:10:28 -0400

    style: cleanup and better temporary benching

--- src/qoi.rs
+++ src/qoi.rs
@@ -1,17 +1,19 @@
-use image::{ColorType, DynamicImage, GenericImageView};
 use std::ops::{Add, Div};
-
-use image::io::Reader;
 use std::path::Path;
 use std::time::{Duration, Instant};
 
-// cargo-asm has spoiled my perceptions of how smart the compiler really is
-// hence my micromanaging exactly which instructions are called when I can
-// I intend to make other platform-specific implementations
-#[cfg_attr(target_feature = "ssse3", path = "hashes/ssse3.rs")]
-#[cfg_attr(not(target_feature = "ssse3"), path = "hashes/slow.rs")]
-mod hashes;
+use image::{ColorType, DynamicImage, GenericImageView};
+use image::io::Reader;
+
 use hashes::hashes_rgba;
+
+// The cargo-asm tool has spoiled my perceptions of how smart the compiler really is,
+// hence my micromanaging exactly which instructions are called at critical parts of the code.
+// My only regret is that it took longer, but the benefits are really good!
+// I intend to make other platform-specific implementations once the base and x86 are finished.
+#[cfg_attr(all(feature = "use_simd", target_feature = "ssse3"), path = "hashes/ssse3.rs")]
+#[cfg_attr(any(not(feature = "use_simd"), not(target_feature = "ssse3")), path = "hashes/slow.rs")]
+mod hashes;
 
 pub fn open_file(path: &str) -> DynamicImage {
     let path = Path::new(path);
@@ -57,23 +59,29 @@ fn raw_to_qoi(bytes: Vec<u8>, colour_type: ColorType, count: usize) {
                 bytes.get(7).expect("Alpha not found"),
                 hashes.get(1).expect("Hash not found")
             );
-            assert_eq!(*hashes.get(0).unwrap(), 54u8);
+            // assert_eq!(*hashes.get(0).unwrap(), 54u8);
 
-            let mut times: Vec<Duration> = Vec::with_capacity(32);
+            let iterations = 1024u32;
 
-            for _ in 0..128 {
+            let mut times: Vec<Duration> = Vec::with_capacity(iterations as usize);
+
+            for iteration in 0..iterations {
                 let timekeeper = Instant::now();
                 let hashes = hashes_rgba(&bytes, count);
                 let duration = timekeeper.elapsed();
                 times.push(duration);
-
-                println!("{:?}: {} hashes", duration, hashes.len());
+                let size_mebibytes = hashes.len() as f64 / 1_048_576.0f64;
+                let rate = size_mebibytes / (duration.as_nanos() as f64 / 1000000000.0f64);
+                println!("Iteration {iteration}: {duration:?} | {rate:.4} MiB/s");
             }
             let mut total_duration: Duration = Duration::from_secs(0);
             for duration in times {
                 total_duration = total_duration.add(duration);
             }
-            println!("Average time: {:?}", total_duration.div(128));
+            let avg_dur = total_duration.div(iterations);
+            let size_mebibytes = hashes.len() as f64 / 1_048_576.0f64;
+            let avg_rate = size_mebibytes / (avg_dur.as_nanos() as f64 / 1000000000.0f64);
+            println!("Average time: {avg_dur:?}   | Average rate: {avg_rate:.4} MiB/s");
         }
         _ => {
             panic!("The {colour_type:?} format is not supported by the QOI format (yet)")

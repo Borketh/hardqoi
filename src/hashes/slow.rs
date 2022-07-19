bc04506dafb5dfed7375b79f48d7ffb00a8a577d
floof <does.not.kompute@gmail.com>
2022-07-18 20:43:18 -0400

    fix(slow): better safety and correctness

--- src/hashes/slow.rs
+++ src/hashes/slow.rs
@@ -1,20 +1,16 @@
-pub fn hashes_rgba(bytes: &Vec<u8>, count: usize) -> Vec<u8> {
-    let safe_iterations = count / 16 + 1;
-    let safe_alloc_bytes = safe_iterations * 16;
-    // Allocate 1-16 bytes extra for the hashes vector, so that writing the final __m128i doesn't
-    // overwrite anything that comes after it and corrupt anything. The capacity should not change,
-    // but the size should be set after writing everything.
-    let mut hashes: Vec<u8> = Vec::with_capacity(safe_alloc_bytes);
+extern crate bytemuck;
+use bytemuck::checked::cast_slice;
 
-    let pixels: &Vec<[u8; 4]> = unsafe { &*(bytes as *const Vec<u8> as *const Vec<[u8; 4]>) };
+pub fn hashes_rgba(bytes: &Vec<u8>, count: usize) -> Vec<u8> {
+    let mut hashes: Vec<u8> = Vec::with_capacity(count);
+
+    let pixels = cast_slice::<u8, [u8; 4]>(bytes);
 
     for i in 0..count {
-
-        let slice: [u8; 4] = pixels[i];
-
-        hashes.push(((slice[0] * 3) + (slice[1] * 5) + (slice[2] * 7) + (slice[3] * 11)) & 0b00111111u8);
+        hashes.push(
+            { (pixels[i][0] * 3) + (pixels[i][1] * 5) + (pixels[i][2] * 7) + (pixels[i][3] * 11) }
+                & 0b00111111u8,
+        );
     }
     return hashes;
 }
-
-

187b73dcdfc2ab989b322f30a15cfd8ac84d4280
floof <does.not.kompute@gmail.com>
2022-07-21 18:30:14 -0400

    chore: reorganize implementations into their own folders

---
+++ src/impl/anything/encoding.rs
@@ -0,0 +1,5 @@
+use crate::qoi::hashing;
+
+pub fn encode_raw_to_qoi(raw: &Vec<u8>, filename: &str) {
+
+}
---
+++ src/impl/x86-ssse3/encoding.rs
@@ -0,0 +1,5 @@
+use crate::qoi::qoi::hashing;
+
+pub fn encode_raw_to_qoi(raw: &Vec<u8>, filename: &str) {
+
+}
